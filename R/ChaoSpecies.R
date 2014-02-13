b9a512cc8db49268bdcbd977b35fcfead6199e88
ddd7975 <ddd7975@gmail.com>
2014-02-13 20:08:39 +0800

    In order to use list by ChaoSpecies

--- R/ChaoSpecies.R
+++ R/ChaoSpecies.R
@@ -14,18 +14,20 @@ function(data, datatype = c("abundance", "incidence"), k = 10, conf = 0.95){
       data <- data[1, ]
     }
   }
-  
+  method <- match.arg(method)
   data <- as.numeric(round(data))
   
   if (datatype == "abundance"){
       f <- function(i, data){length(data[which(data == i)])}
       if (f(1, data) == sum(data)){
         stop("Error: The information of data is not enough.")}
-    SpeciesAbundance(data, method = "all", k = k, conf = conf)
+      return(list(BASIC.DATA.INFORMATION = basicAbun(data, k)[[1]], Rare.Species.Group = RareSpeciesGroup(data, k), 
+                  SPECIES.TABLE = round(SpecAbunOut(data, method, k, conf), 3)))
     } else {
       dat <- data[-1]; Q <- function(i, data){length(data[which(data == i)])}
       if (Q(1, dat) == sum(dat)){
         stop("Error: The information of data is not enough.")}
-    SpeciesIncidence(data, method = "all", k = k, conf = conf)  
+      return(list(BASIC.DATA.INFORMATION = basicInci(data, k)[[1]], SPECIES.TABLE = SpecInciOut(data, method, k, conf)))
     }
+  return()
 }
